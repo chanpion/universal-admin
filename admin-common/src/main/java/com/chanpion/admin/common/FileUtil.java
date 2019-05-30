adae90c165033e132f98e60f0d4682dad5845355
April.Chen <forchpeng@163.com>
2019-05-30 17:19:40 +0800

    初始提交

---
+++ admin-common/src/main/java/com/chanpion/admin/common/FileUtil.java
@@ -0,0 +1,8 @@
+package com.chanpion.admin.common;
+
+/**
+ * @author April Chen
+ * @date 2019/5/30 17:18
+ */
+public class FileUtil {
+}
---
+++ admin-system/src/main/java/com/chanpion/admin/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.chanpion.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Chen
+ * @date 2019/5/30 16:58
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
