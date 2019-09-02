86760d1db513a0ce8e6affde9072ac84a63d729e
April.Chen <forchpeng@163.com>
2019-09-02 16:32:29 +0800

    初始提交

---
+++ admin-web/src/main/java/com/chanpion/admin/AdminWebApplication.java
@@ -0,0 +1,16 @@
+package com.chanpion.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Chen
+ * @date 2019/9/2 16:00
+ */
+@SpringBootApplication
+public class AdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminWebApplication.class, args);
+    }
+}
