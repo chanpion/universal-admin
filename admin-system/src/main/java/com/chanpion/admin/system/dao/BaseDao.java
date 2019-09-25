e570a21115191947f381e560c36bb36aa3b9daa3
April.Chen <forchpeng@163.com>
2019-09-25 20:24:01 +0800

    用户管理

--- admin-system/src/main/java/com/chanpion/admin/system/dao/BaseDao.java
+++ admin-system/src/main/java/com/chanpion/admin/system/dao/BaseDao.java
@@ -1,9 +1,41 @@
 package com.chanpion.admin.system.dao;
 
+import java.util.List;
+
 /**
  * @author April Chen
  * @date 2019/9/6 15:58
  */
 public interface BaseDao<T> {
 
+    /**
+     * 插入一条数据
+     *
+     * @param item
+     * @return 成功条数
+     */
+    Integer insert(T item);
+
+    /**
+     * 更新记录
+     *
+     * @param item
+     * @return
+     */
+    Integer update(T item);
+
+    /**
+     * 查询一条数据
+     *
+     * @param item
+     * @return
+     */
+    T findOne(T item);
+
+    /**
+     *
+     * @param item
+     * @return
+     */
+    List<T> find(T item);
 }
